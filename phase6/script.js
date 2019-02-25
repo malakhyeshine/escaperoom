383de0ef6ee6211d5a7894fd6f67efdba72bec34
malakhyeshine <26973202+malakhyeshine@users.noreply.github.com>
2019-02-25 08:35:12 -0800

    Add files via upload

---
+++ phase6/script.js
@@ -0,0 +1,65 @@
+function reappearImage(el) {
+  var element = document.getElementById('alien1');
+  element.classList.add('reappear')
+}
+
+function hideImage(el) {
+  var element = document.getElementById("alien1");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage, 3000);
+}
+
+function reappearImage2(el) {
+  var element = document.getElementById('alien2');
+  element.classList.add('reappear')
+}
+
+function hideImage2(el) {
+  var element = document.getElementById("alien2");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage2, 3000);
+}
+
+function reappearImage3(el) {
+  var element = document.getElementById('alien3');
+  element.classList.add('reappear')
+}
+
+function hideImage3(el) {
+  var element = document.getElementById("alien3");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage3, 3000);
+}
+
+function reappearImage4(el) {
+  var element = document.getElementById('alien4');
+  element.classList.add('reappear')
+}
+
+function hideImage4(el) {
+  var element = document.getElementById("alien4");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage4, 3000);
+}
+
+function reappearImage5(el) {
+  var element = document.getElementById('alien5');
+  element.classList.add('reappear')
+}
+
+function hideImage5(el) {
+  var element = document.getElementById("alien5");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage5, 3000);
+}
+
+function reappearImage6(el) {
+  var element = document.getElementById('alien6');
+  element.classList.add('reappear')
+}
+
+function hideImage6(el) {
+  var element = document.getElementById("alien6");
+  element.classList.add('hideimage')
+  setTimeout (reappearImage6, 3000);
+}
